8ce7a71a00efc4693abd37bebfea67b8638c3f06
anshulrastogi <anshulrastogi@IN-FVFHM1KDQ05P.local>
2023-09-02 15:57:44 +0530

    added files

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,13 @@ chart_data = pd.DataFrame(
     np.random.randn(20,3), columns=["a", "b", "c"]
 )
 
+map_data = pd.DataFrame(
+    np.random.randn(1000,2) /[50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon']
+)
+
+st.map(map_data)
+
 st.line_chart(chart_data)
 # df = pd.read_csv("my_data.csv")
 # st.bar_chart(df)
