192bc3dd5786072a2642ccf43b2b3976be607947
anshulrastogi <anshulrastogi@IN-FVFHM1KDQ05P.local>
2023-09-02 15:54:56 +0530

    added files

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import numpy as np
 import pandas as pd
 
 st.write("""
@@ -7,5 +7,12 @@ st.write("""
 Hello *world!*
 """)
          
-df = pd.read_csv("my_data.csv")
-st.bar_chart(df)
+chart_data = pd.DataFrame(
+    np.random.randn(20,3), columns=["a", "b", "c"]
+)
+
+st.line_chart(chart_data)
+# df = pd.read_csv("my_data.csv")
+# st.bar_chart(df)
+
+
